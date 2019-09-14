f75e4abcffec770845a3095baab4705e1a71a39e
Hà Thái Hoàng <hoanght@bap.jp>
2019-09-14 12:12:17 +0700

    sdf

--- src/overwrite/antd-icons.js
+++ src/overwrite/antd-icons.js
@@ -1,18 +1,10 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import LoadingOutline from '@ant-design/icons/lib/outline/LoadingOutline'
-import MinusSquareOutline from '@ant-design/icons/lib/outline/MinusSquareOutline'
-import PlusSquareOutline from '@ant-design/icons/lib/outline/PlusSquareOutline'
-import FileOutline from '@ant-design/icons/lib/outline/FileOutline'
-import CloseCircleFill from '@ant-design/icons/lib/fill/CloseCircleFill'
-import DownOutline from '@ant-design/icons/lib/outline/DownOutline'
-import MenuOutline from '@ant-design/icons/lib/outline/MenuOutline'
-
-export {
-  LoadingOutline,
-  CloseCircleFill,
-  MinusSquareOutline,
-  PlusSquareOutline,
-  FileOutline,
-  DownOutline,
-  MenuOutline
-}
+export LoadingOutline from '@ant-design/icons/lib/outline/LoadingOutline'
+export MinusSquareOutline from '@ant-design/icons/lib/outline/MinusSquareOutline'
+export PlusSquareOutline from '@ant-design/icons/lib/outline/PlusSquareOutline'
+export FileOutline from '@ant-design/icons/lib/outline/FileOutline'
+export CloseCircleFill from '@ant-design/icons/lib/fill/CloseCircleFill'
+export DownOutline from '@ant-design/icons/lib/outline/DownOutline'
+export HomeOutline from '@ant-design/icons/lib/outline/HomeOutline'
+export UpOutline from '@ant-design/icons/lib/outline/UpOutline'
+export StarOutline from '@ant-design/icons/lib/outline/StarOutline'
+export CaretDownFill from '@ant-design/icons/lib/fill/CaretDownFill'
