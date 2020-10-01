0b07a604e70ac6fc6cccc6ea3fb3482bdc8eaa10
Hà Thái Hoàng <jonlefcun@gmail.com>
2020-10-01 09:53:13 +0700

    Update loading.js

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,8 +1,11 @@
+import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 import { Colors } from '@/theme'
 
-export default styled.div`
+const StyledDiv = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
@@ -20,4 +23,25 @@ export default styled.div`
     border-right-color: ${({ theme }) => Colors.alpha(theme.primary, 0.5)};
     animation: rotation 1s infinite linear;
   }
+  
+  &.small {
+    &::after {
+      width: 25px;
+      height: 25px;
+      border: 4px solid ${({ theme }) => theme.primary};
+      border-left-color: ${({ theme }) => Colors.alpha(theme.primary, 0.5)};
+      border-right-color: ${({ theme }) => Colors.alpha(theme.primary, 0.5)};
+    }
+  }
 `
+
+const Loading = ({ size }) => (
+  <StyledDiv
+    className={classnames(size)}
+  />
+)
+Loading.propTypes = {
+  size: PropTypes.oneOf(['small'])
+}
+
+export default Loading
