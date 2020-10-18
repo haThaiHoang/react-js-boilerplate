629bcc8b336db9b98fd9414da637f4089ca9ed4d
Hà Thái Hoàng <jonlefcun@gmail.com>
2020-10-18 11:05:03 +0700

    Update date-picker.js

--- src/components/date-picker.js
+++ src/components/date-picker.js
@@ -1,21 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { DatePicker as AntdDatePicker } from 'antd'
 import styled from 'styled-components'
+import classnames from 'classnames'
+import { DatePicker } from 'antd'
+import moment from 'moment'
+import lodash from 'lodash'
 
-const StyledDatePicker = styled(AntdDatePicker)`
-  /* stylelint-disable */
+const StyledDiv = styled.div`
+  &.error {
+    .ant-picker {
+      border: solid 1px red;
+    }
+  }
 `
 
-const DatePicker = ({ field, form, ...props }) => (
-  <StyledDatePicker
-    {...field}
-    {...props}
-  />
-)
-DatePicker.propTypes = {
-  field: PropTypes.object,
-  form: PropTypes.object
+class DatePicker extends Component {
+  static propTypes = {
+    field: PropTypes.object,
+    form: PropTypes.object,
+    value: PropTypes.instanceOf(Date),
+    onChange: PropTypes.func,
+    inputOutputFormat: PropTypes.string
+  }
+
+  _onChange = (date) => {
+    const { field, form, onChange, inputOutputFormat } = this.props
+
+    date = inputOutputFormat ? date?.format(inputOutputFormat) : date
+
+    if (form && field) form.setFieldValue(field.name, date)
+    if (onChange) onChange(date)
+  }
+
+  render() {
+    const { field, form, className, inputOutputFormat, ...props } = this.props
+    let { value } = this.props
+
+    value = field?.value || value
+    value = value && moment(value, inputOutputFormat)
+
+    return (
+      <StyledDiv
+        className={classnames({ error: lodash.get(form, `errors.${field?.name}`) }, className)}
+      >
+        <DatePicker
+          {...props}
+          onChange={this._onChange}
+          value={value}
+        />
+      </StyledDiv>
+    )
+  }
 }
 
 export default DatePicker
