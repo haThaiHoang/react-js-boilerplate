e64dcb46580d64e7369768f1f01150b9df99df70
Hà Thái Hoàng <jonlefcun@gmail.com>
2020-10-18 11:07:19 +0700

    Create clickable.js

---
+++ src/components/clickable.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  cursor: pointer;
+  user-select: none;
+  transition: opacity 0.2s;
+  
+  &:hover {
+    opacity: 0.8;
+  }
+  
+  &:active {
+    opacity: 0.7;
+  }
+`
+
+const Clickable = ({ children, className, onClick }) => (
+  <StyledDiv
+    className={className}
+    onClick={onClick}
+  >
+    {children}
+  </StyledDiv>
+)
+Clickable.propTypes = {
+  onClick: PropTypes.func
+}
+
+export default Clickable
