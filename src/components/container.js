84f572abb9694f79033af2674a81cda027b5713c
Hà Thái Hoàng <jonlefcun@gmail.com>
2020-10-18 11:08:40 +0700

    Update container.js

--- src/components/container.js
+++ src/components/container.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
+import classNames from 'classnames'
+
+import Media from '@/utils/media'
 
 const Div = styled.div`
-  padding: 20px 20px 20px 20px;
+  max-width: 1140px;
+  box-sizing: border-box;
+  margin: 0 auto;
+  padding: 0 15px;
   width: 100%;
+  
+  ${Media.lessThan(Media.SIZE.XL)} {
+    max-width: 960px;
+  }
+  
+  ${Media.lessThan(Media.SIZE.LG)} {
+    max-width: 720px;
+  }
+  
+  ${Media.lessThan(Media.SIZE.MD)} {
+    max-width: 540px;
+  }
+  
+  ${Media.lessThan(Media.SIZE.SM)} {
+    max-width: initial;
+  }
 `
 
-export default ({ children, ...props }) => (
-  <Div {...props}>{ children }</Div>
-)
+export default function ({ children, className }) {
+  className = classNames(className, 'container')
+
+  return (
+    <Div className={className}>{ children }</Div>
+  )
+}
