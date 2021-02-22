103f0d3627c38c412823632a85dfe709f826f33c
Ha Thai Hoang <hoanght@bap.jp>
2021-02-22 13:18:16 +0700

    Master

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,7 +1,4 @@
 import '@/resources/styles/preloading.less'
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-import 'whatwg-fetch'
 
 // eslint-disable-next-line no-unused-expressions
 import('./app')
