1590ff7f78d363b55f0f8af7837253ab97222972
Ha Thai Hoang <hoanght@bap.jp>
2021-02-23 16:12:15 +0700

    Master

--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import lodash from 'lodash'
 
+import Storage from '@/utils/storage'
 import common from './common.json'
 import home from './home.json'
 
@@ -12,10 +13,15 @@ const resources = lodash.merge(
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'en',
+  lng: Storage.get('LANGUAGE') || 'en',
   interpolation: {
     escapeValue: false
   }
+})
+
+i18n.off('languageChanged')
+i18n.on('languageChanged', (language) => {
+  Storage.set('LANGUAGE', language)
 })
 
 export default i18n
