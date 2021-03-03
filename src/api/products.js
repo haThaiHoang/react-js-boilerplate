1aa10685e513533eda184f01b1573d4d7ba2002f
Ha Thai Hoang <hoanght@bap.jp>
2021-03-03 09:59:50 +0700

    Master

--- src/api/products.js
+++ src/api/products.js
@@ -8,92 +8,92 @@ export function getProducts() {
     data: {
       products: [{
         id: 1,
-        avatar: 'https://image.shutterstock.com/image-photo/red-apple-on-white-background-260nw-158989157.jpg',
+        avatar: 'https://picsum.photos/id/1/300',
         name: 'Apple',
         description: 'Ngon'
       }, {
         id: 2,
-        avatar: 'https://suttons.s3.amazonaws.com/p/VECAR14605rt_3.jpg',
+        avatar: 'https://picsum.photos/id/2/300',
         name: 'Carrot',
         description: 'Quá Ngon'
       }, {
         id: 3,
-        avatar: 'http://elmatadorrestaurant.com/wp-content/uploads/2017/12/MILK.jpg',
+        avatar: 'https://picsum.photos/id/3/300',
         name: 'Milk',
         description: 'Hơi béo'
       }, {
         id: 4,
-        avatar: 'https://i.ytimg.com/vi/SX6TXkoEjMo/maxresdefault.jpg',
+        avatar: 'https://picsum.photos/id/4/300',
         name: 'Orange',
         description: 'Chua'
       }, {
         id: 5,
-        avatar: 'https://www.healthline.com/hlcmsresource/images/AN_images/orange-juice-732x549-thumbnail.jpg',
+        avatar: 'https://picsum.photos/id/5/300',
         name: 'Orange juice',
         description: 'Quá Tuyệt'
       }, {
         id: 6,
-        avatar: 'https://www.brandsvietnam.com/upload/news/480px/2014/cocacola-ava-ID5733_1418099201.jpg',
+        avatar: 'https://picsum.photos/id/6/300',
         name: 'Cocacola',
         description: 'Hơi ngọt'
       }, {
         id: 7,
-        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT92UhHqSnPKVLNmdziS7o95kgEp62oDRKj0hhrEhU8GByE4ybb&s',
+        avatar: 'https://picsum.photos/id/7/300',
         name: 'Candy',
         description: 'Ngọt'
       }, {
         id: 8,
-        avatar: 'https://www.healthline.com/hlcmsresource/images/AN_images/orange-juice-732x549-thumbnail.jpg',
+        avatar: 'https://picsum.photos/id/8/300',
         name: 'Candy',
         description: 'Ngọt'
       }, {
         id: 9,
-        avatar: 'https://www.brandsvietnam.com/upload/news/480px/2014/cocacola-ava-ID5733_1418099201.jpg',
+        avatar: 'https://picsum.photos/id/9/300',
         name: 'Cocacola',
         description: 'Hơi ngọt'
       }, {
         id: 11,
-        avatar: 'https://image.shutterstock.com/image-photo/red-apple-on-white-background-260nw-158989157.jpg',
+        avatar: 'https://picsum.photos/id/11/300',
         name: 'Apple',
         description: 'Ngon'
       }, {
         id: 12,
-        avatar: 'https://suttons.s3.amazonaws.com/p/VECAR14605rt_3.jpg',
+        avatar: 'https://picsum.photos/id/12/300',
         name: 'Carrot',
         description: 'Quá Ngon'
       }, {
         id: 13,
-        avatar: 'http://elmatadorrestaurant.com/wp-content/uploads/2017/12/MILK.jpg',
+        avatar: 'https://picsum.photos/id/13/300',
         name: 'Milk',
         description: 'Hơi béo'
       }, {
         id: 14,
-        avatar: 'https://i.ytimg.com/vi/SX6TXkoEjMo/maxresdefault.jpg',
+        avatar: 'https://picsum.photos/id/14/300',
         name: 'Orange',
         description: 'Chua'
       }, {
         id: 15,
-        avatar: 'https://www.healthline.com/hlcmsresource/images/AN_images/orange-juice-732x549-thumbnail.jpg',
+        avatar: 'https://picsum.photos/id/15/300',
         name: 'Orange juice',
         description: 'Quá Tuyệt'
       }, {
         id: 16,
-        avatar: 'https://www.brandsvietnam.com/upload/news/480px/2014/cocacola-ava-ID5733_1418099201.jpg',
+        avatar: 'https://picsum.photos/id/16/300',
         name: 'Cocacola',
         description: 'Hơi ngọt'
       }, {
         id: 17,
-        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT92UhHqSnPKVLNmdziS7o95kgEp62oDRKj0hhrEhU8GByE4ybb&s',
+        avatar: 'https://picsum.photos/id/17/300',
         name: 'Candy',
         description: 'Ngọt'
       }, {
         id: 18,
-        avatar: 'https://www.healthline.com/hlcmsresource/images/AN_images/orange-juice-732x549-thumbnail.jpg',
+        avatar: 'https://picsum.photos/id/18/300',
         name: 'Candy',
         description: 'Ngọt'
       }, {
         id: 19,
-        avatar: 'https://www.brandsvietnam.com/upload/news/480px/2014/cocacola-ava-ID5733_1418099201.jpg',
+        avatar: 'https://picsum.photos/id/19/300',
         name: 'Cocacola',
         description: 'Hơi ngọt'
       }],
